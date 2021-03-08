9f40ec44d8aa28a67dcdce50b6e1465f70b6d96f
Omari Grampus <ojgrampus@gmail.com>
2021-03-08 18:14:25 -0500

    added more css styling and started js scripting

---
+++ script.js
@@ -0,0 +1,7 @@
+// Getting all required elements
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+
+// If start Quioz Button Clicked
